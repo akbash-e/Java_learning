7bf33af6efe012ce3991a3d10856fc08f85058c5
MoonHedgehog <akbash_e@mail.ru>
2026-07-13 22:01:56 +0300

    Добавлена первая программа: вывод имён семьи

--- src/section1_introduction/intro.java
+++ src/section1_introduction/intro.java
@@ -1,4 +1,10 @@
 package section1_introduction;
 
-public class intro {
+public class Intro {
+    public static void main(String[] args){
+        System.out.println("Father");
+        System.out.println("Sister");
+        System.out.println("I am");
+        System.out.println("Mar");
+    }
 }
