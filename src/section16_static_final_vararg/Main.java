03502c867b7f5de416253aa5512e9310a7450868
MoonHedgehog <akbash_e@mail.ru>
2026-08-08 01:37:47 +0300

    Примеры из раздела Ключевые слова static, final и аргументы переменной длины

---
+++ src/section16_static_final_vararg/Main.java
@@ -0,0 +1,8 @@
+package section16_static_final_vararg;
+
+public class Main {
+    static void main() {
+        System.out.println(MyMath.length(10));
+        System.out.println(MyMath.sum(1, 2, 3, 4, 5));
+    }
+}
---
+++ src/section16_static_final_vararg/MyMath.java
@@ -0,0 +1,25 @@
+package section16_static_final_vararg;
+
+public class MyMath {
+    public static final double PI = 3.14;
+
+    public static int sum(int... numbers) {
+        int sum = 0;
+        for (int number : numbers) {
+            sum += number;
+        }
+        return sum;
+    }
+    public static double length(double radius) {
+        return 2 * PI * radius;
+    }
+    public static int multiple(int a, int b) {
+        return a*b;
+    }
+    public static double multiple(double a, double b) {
+        return a*b;
+    }
+    public static int multiple(int a) {
+        return a*a;
+    }
+}
